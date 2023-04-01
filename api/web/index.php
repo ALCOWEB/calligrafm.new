423d6557127af5d7bb6c4ade262d67536dcdcc37
KMochalov <KMochalov@srt-llc.ru>
2023-04-01 14:19:08 +0300

    change nginx conf

--- api/web/index.php
+++ api/web/index.php
@@ -1,24 +1,22 @@
 <?php
 
-//defined('YII_DEBUG') or define('YII_DEBUG', true);
-//defined('YII_ENV') or define('YII_ENV', 'dev');
-//
-//require __DIR__ . '/../../vendor/autoload.php';
-//require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
-//require __DIR__ . '/../../common/config/bootstrap.php';
-//require __DIR__ . '/../config/bootstrap.php';
-//
-//$config = yii\helpers\ArrayHelper::merge(
-//    require __DIR__ . '/../../common/config/main.php',
-//    require __DIR__ . '/../../common/config/main-local.php',
-//    require __DIR__ . '/../config/main.php',
-//    require __DIR__ . '/../config/main-local.php'
-//);
-//
-//(new yii\web\Application($config))->run();
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
-declare(strict_types=1);
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../../common/config/bootstrap.php';
+require __DIR__ . '/../config/bootstrap.php';
 
-header('Content-Type: application/json');
+$config = yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../config/main.php',
+    require __DIR__ . '/../config/main-local.php'
+);
 
-echo '{}';
+(new yii\web\Application($config))->run();
+
+//header('Content-Type: application/json');
+//
+//echo '{}';
